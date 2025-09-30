d496b2e2e527eb1e295c7310e3495e712e659501
firebolt778 <firebolt77800@gmail.com>
2025-09-30 10:02:55 +0900

    Add next-sitemap configuration and generate sitemap and robots.txt files

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+    siteUrl: 'https://vexengineering.com', // your live domain
+    generateRobotsTxt: true, // generates robots.txt as well
+    sitemapSize: 7000, // optional, splits if you have >7000 URLs
+};
